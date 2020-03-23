07b61fd13d39d5f21a246a16809ba5ddd64f4e1f
mbobas <michal.gulczynski88@gmail.com>
2020-03-23 21:36:12 +0100

    Updates

---
+++ precache-manifest.115b5559dc22c1df00fd413ba45e6edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7da20ea53731bbbc416ca8f14fb240b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "688692fcd89f38be5053",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "53a019a6f24d9ebbc0c3",
+    "url": "/react-portfolio/static/js/2.87c072e8.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/react-portfolio/static/js/2.87c072e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688692fcd89f38be5053",
+    "url": "/react-portfolio/static/js/main.77f28831.chunk.js"
+  },
+  {
+    "revision": "2d861790c9ce2e05a198",
+    "url": "/react-portfolio/static/js/runtime-main.73e50512.js"
+  }
+]);
