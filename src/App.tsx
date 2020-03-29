f58bba752c237b079a0a703b9835e42d65d07a15
mbobas <michal.gulczynski88@gmail.com>
2020-03-29 20:36:46 +0200

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,74 @@
+  
 import './App.css';
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import About from './pages/About';
+import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin-top: 20px;
+`;
+
+const MenuWrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  background-color: white;
+  border-color: grey;
+  border: 2px;
+  margin-top: 20px;
+  margin-right: 5vw;
+  margin-left: 5vw;
+  @media only screen and (max-width: 400px) {
+    margin-right: 10px;
+  }
+  @media only screen and (max-width: 600px) {
+  }
+`;
+
+const MenuLink = styled.p`
+  font-family: 'Exo';
+  margin-left: 15px; 
+  color: #141c3a;
+  font-weight: 600;
+`;
+
+const SayButton = styled.button`
+    margin-left: 10px;
+    border-width: 2px;
+    font-weight: 400;
+    font-size: 18px;
+    padding: 12px;
+    background-color: transparent;
+    border-color: #7510F7;
+    color: #7510F7;
+    border-radius: 50px;
+    font-family: 'Exo';
+    outline: none;
+    :hover {
+      background-color: #7510F7;
+      color: white;
+    }
+`;
 
 class App extends Component {
   render() {
     return (
+  <Wrapper>
     <Router>
-        <div>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li><Link to={'/'} className="nav-link"> Home </Link></li>
-            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
-            <li><Link to={'/about'} className="nav-link">About</Link></li>
-          </ul>
-          </nav>
-          <hr />
+        <MenuWrapper>
+        <Link to={'/contact'} ><SayButton>Say Hello</SayButton></Link>
+        <Link to={'/projects'} ><MenuLink>Projects</MenuLink></Link>
+        <Link to={'/'} > <MenuLink>Home</MenuLink> </Link>
+        </MenuWrapper>
           <Switch>
               <Route exact path='/' component={Home} />
+              <Route path='/projects' component={Projects} />
               <Route path='/contact' component={Contact} />
-              <Route path='/about' component={About} />
           </Switch>
-        </div>
       </Router>
+  </Wrapper>
     );
   }
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,60 +1,144 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import ProjectBox from './ProjectBox';
+import autor from '../../assets/autor.png';
+import singlescreen from '../../assets/singlescreen.png';
+import myAvatarPNGSMALL from '../../assets/myAvatarPNGSMALL.png';
+import myAvatarPNG300 from '../../assets/myAvatarPNG300.png';
+import Background from '../../assets/background.png';
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  bacgroudnd-color: white;
-`;
+  background-color: white;
+  justify-content: center;
 
+  `;
 const Header = styled.div`
   display: flex;
-  justify-content: center;
-  font-size: 30px;
-  height: 700px;
-  background-color: #333;
+  flex-direction: column;
+  justify-content: flex-start;
+  height: 100vh;
+  background-color: #FFF;
 `;
-
-const HeaderText = styled.h1`
+//background-image: url(${Background});
+  ///background-color: #333;
+const HeaderText = styled.div`
+  margin-top: 15vh;
+  width: 99%;
+  font-family: 'Exo';
+  font-weight: 800;
   font-size: 48px;
-  font-weight: 500;
   line-height: 60px;
   text-align: center;
-  color: #fff;
+  color: #141c3a;
+  @media only screen and (max-width: 400px) {
+    margin-top: 5vh;
+    font-size: 30px;
+  }
+  @media only screen and (max-width: 600px) {
+    font-size: 30px;
+  }
+`;
+
+const SubHeaderText = styled.div`
+  width: 99%;
+  font-family: 'Exo';
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 25px;
+  text-align: center;
+  color: #141c3a;
+  @media only screen and (max-width: 400px) {
+    font-size: 16px;
+    line-height: 25px;
+  }
+  @media only screen and (max-width: 600px) {
+    font-size: 18px;
+    line-height: 25px;
+  }
+`;
+
+
+// /text-align: center;
+const HeaderImage = styled.img`
+  margin-top: 15vh;
+  border-radius: 50%;
+  height: 300px;
+  width: 300px;
+  background-image: url(${myAvatarPNG300});
+  box-shadow: 5px 5px 15px 5px #00000071;
+  align-self: center;
+  @media only screen and (max-width: 400px) {
+    margin-top: 7vh;
+  }
+  @media only screen and (max-width: 600px) {
+    height: 200px;
+    width: 200px;
+    background-image: url(${myAvatarPNGSMALL});
+  }
 `;
 
 const ProjectWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-between;
+  align-self: center;
 `;
+
+const AboutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-self: center;
+  border
+`;
+
 const ProjectHeader = styled.p`
   align-self: center;
   font-size: 40px;
   color: #333;
 `;
 
+
+
+
+const name = '<CV> Michal Gulczynski </CV>';
+const title = 'React Native and React, Mobile & Front-end Developer ';
+const subtitle = 'I like creates dreams true, and I love this work.'
+const 
 class Home extends Component {
   render() {
     return (
         <Wrapper>
           <Header>
-            <HeaderText>Home</HeaderText>
+            <HeaderText> {title} </HeaderText>
+            <SubHeaderText> {subtitle} </SubHeaderText>
+            <HeaderImage></HeaderImage>
           </Header>
+          <ProjectHeader>About</ProjectHeader>
+          <AboutWrapper>
+            <ProjectBox
+            projectname="Nextflix"
+            projecttext="test"
+            image={singlescreen}
+            />
+          </AboutWrapper>
           <ProjectHeader>Projects</ProjectHeader>
           <ProjectWrapper>
               <ProjectBox
                 projectname="Nextflix"
                 projecttext="test"
+                image={singlescreen}
               />
               <ProjectBox
                 projectname="Nextflix"
                 projecttext="test"
+                image={singlescreen}
               />
               <ProjectBox
                 projectname="Nextflix"
                 projecttext="test"
+                image={singlescreen}
               />
           </ProjectWrapper>
           
